2775a7c7d8973172ac79aac32947130c7c293f39
paka0401 <6qocwNBG5i7Vgf7HqengaV>
2020-12-07 13:31:01 +0100

    Fixed UserWarning (This overload of nonzero is deprecated) in lib/loss/sampler.py

--- 2_metric-learning-divide-and-conquer/lib/data/loader/utils.py
+++ 2_metric-learning-divide-and-conquer/lib/data/loader/utils.py
@@ -95,5 +95,5 @@ def merge(dls_non_iter):
                 # initialize new dataloader in case no batches left
                 dls[i] = iter(dls_non_iter[i])
                 b = next(dls[i])
-            yield b, dls[i].dataset
+            yield b, dls[i]._dataset # changed .dataset to ._dataset (there was no attribute 'dataset')
 
