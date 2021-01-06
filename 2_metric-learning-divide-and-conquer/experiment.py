c4506001446f46dc0f9660275f570c52976c0a63
paka0401 <6qocwNBG5i7Vgf7HqengaV>
2021-01-06 11:36:01 +0100

    script change

--- 2_metric-learning-divide-and-conquer/experiment.py
+++ 2_metric-learning-divide-and-conquer/experiment.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     DIYlogger.setLevel(logging.INFO)
     _FMT_STRING = '[%(levelname)s:%(asctime)s] %(message)s'
     _DATE_FMT = '%Y-%m-%d %H:%M:%S'
-    file_handler = logging.FileHandler(pj_base_path+"log/diy_log.txt", mode='w')
+    file_handler = logging.FileHandler(pj_base_path+"/log/diy_log.txt", mode='w+')
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(logging.Formatter(_FMT_STRING, datefmt=_DATE_FMT))
     DIYlogger.addHandler(file_handler)
