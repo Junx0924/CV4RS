90d3bbd39bf752e381fbcd38f2bad3a42ce5447f
paka0401 <6qocwNBG5i7Vgf7HqengaV>
2021-01-06 22:15:49 +0100

    changed script

--- 2_metric-learning-divide-and-conquer/run_mlrsnet.sh
+++ 2_metric-learning-divide-and-conquer/run_mlrsnet.sh
@@ -22,4 +22,6 @@ module load nvidia/cuda/10.0
 export OMP_NUM_THREADS=1
 export USE_SIMPLE_THREADED_LEVEL3=1
 
+python /home/users/p/paka0401/CV4RS/CV4RS/2_metric-learning-divide-and-conquer/logging_test.py
+
 python /home/users/p/paka0401/CV4RS/CV4RS/2_metric-learning-divide-and-conquer/experiment.py --dataset=mlrsnet --nb-clusters=60 --sz-embedding=4096 --backend=faiss-gpu
