4e13c61d5d467d87e343427112b427ea1f7e1677
paka0401 <6qocwNBG5i7Vgf7HqengaV>
2021-01-07 14:20:08 +0100

    changed scripts

--- 2_metric-learning-divide-and-conquer/run_bigearthnet.sh
+++ 2_metric-learning-divide-and-conquer/run_bigearthnet.sh
@@ -17,7 +17,6 @@
 
 export LD_LIBRARY_PATH=/home/users/p/paka0401/lib
 source /home/users/p/paka0401/divide_and_conquer_venv/bin/activate
-module load python/3.7.1
 module load nvidia/cuda/10.0
 export OMP_NUM_THREADS=1
 export USE_SIMPLE_THREADED_LEVEL3=1
--- 2_metric-learning-divide-and-conquer/run_print_test.sh
+++ 2_metric-learning-divide-and-conquer/run_print_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH -J D&C_mlrsnet
-#SBATCH -o /home/users/p/paka0401/CV4RS/CV4RS/2_metric-learning-divide-and-conquer/log/mlrsnet_log.out
+#SBATCH -o /home/users/p/paka0401/CV4RS/CV4RS/2_metric-learning-divide-and-conquer/log/print_test.out
 
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
@@ -17,9 +17,8 @@
 
 export LD_LIBRARY_PATH=/home/users/p/paka0401/lib
 source /home/users/p/paka0401/divide_and_conquer_venv/bin/activate
-module load python/3.7.1
 module load nvidia/cuda/10.0
 export OMP_NUM_THREADS=1
 export USE_SIMPLE_THREADED_LEVEL3=1
 
-python print('something')
+python -c "print('something')"
