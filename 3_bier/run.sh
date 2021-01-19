a4c6ad9d531fb979b9ec2065085a0adafb1956f4
junx0924 <glamorbree@gmail.com>
2021-01-19 16:10:03 +0100

    update

--- 3_bier/run.sh
+++ 3_bier/run.sh
@@ -2,13 +2,13 @@ TF_LOG_PATH=/media/nax/tensor-bier/products_new_release
 mkdir -p ${TF_LOG_PATH}
 
 CUDA_VISIBLE_DEVICES="0" python -u train_bier.py \
-    --train-images /data/metric/bier/datasets/train_images_products.npy \
-    --train-labels /data/metric/bier/datasets/train_labels_products.npy \
+    --dataset_name BigEarthNet \
+    --source_path /scratch/CV4RS/Dataset\
     --lr-anneal 25000 \
     --lr-decay 0.5 \
     --num-iterations 100000 \
     --labels-per-batch 64 \
-    --images-per-identity 2 \
+    --samples_per_class 2 \
     --batch-size 128 \
     --regularization adversarial \
     --skip-test \
