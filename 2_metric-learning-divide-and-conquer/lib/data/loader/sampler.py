ea7bcb606db15acf9c537890a5eb2c88ab97aae3
junx0924 <glamorbree@gmail.com>
2021-01-19 15:25:07 +0100

    update batchminer (use distance weighted)

--- 2_metric-learning-divide-and-conquer/lib/data/loader/sampler.py
+++ 2_metric-learning-divide-and-conquer/lib/data/loader/sampler.py
@@ -2,72 +2,36 @@ from __future__ import print_function
 from __future__ import division
 
 import logging
+import torch
 import numpy as np
-from torch.utils.data.sampler import BatchSampler
+import random
 
-
-class ClassBalancedSampler(BatchSampler):
+class ClassBalancedSampler(torch.utils.data.sampler.Sampler):
     """
     Sampler that generates class balanced indices with classes chosen randomly.
-    For example, choosing batch_size = 32 and nun_samples_per_class = 8
+    For example, choosing batch_size = 50 and nun_samples_per_class = 2
     will result in
-    32 indices, which point to 8 samples from 32/8=4 randomly picked classes.
+    50 indices, which point to 2 samples from 50/2=25 randomly picked classes.
     """
 
-    def __init__(self, dataset, batch_size=80, num_samples_per_class=4):
-        self.duplicate_small_classes = True
-        assert batch_size % num_samples_per_class == 0, \
-                "batch size must be divisable by num_samples_per_class"
-        self.targets = np.array(dataset.ys)
-        self.C = list(set(self.targets))
-        self.C_index = {
-            c: np.where(self.targets == c)[0] for c in self.C}
-        for c in self.C:
-            np.random.shuffle(self.C_index[c])
-        self.C_count = {c: 0 for c in self.C}
-        self.count = 0
-        self.num_classes = batch_size // num_samples_per_class
-        self.num_samples_per_class = num_samples_per_class
-        self.dataset = dataset
-        self.batch_size = batch_size
+    def __init__(self, image_dict, image_list,batch_size=50, num_samples_per_class=2):
+        self.image_dict         = image_dict
+        self.image_list         = image_list
+
+        self.batch_size         = batch_size
+        self.samples_per_class  = num_samples_per_class
+        self.sampler_length     = len(image_list)//batch_size
+        assert self.batch_size%self.samples_per_class==0, '#Samples per class must divide batchsize!'
 
     def __iter__(self):
-        self.count = 0
-        is_not_enough_classes = len(self.C) < self.num_classes
-        if is_not_enough_classes:
-            logging.warning(('Not enough classes to sample batches: have={},'
-                         'required={}').format(len(self.C), self.num_classes))
-        while self.count + self.batch_size < len(self.dataset):
-            C = np.random.choice(
-                self.C, self.num_classes, replace=is_not_enough_classes
-            )
-            indices = []
-            for class_ in C:
-                if self.C_count[class_] + self.num_samples_per_class\
-                   > len( self.C_index[class_]):
-                    indices.extend(
-                        np.random.choice(
-                            self.C_index[class_],
-                            self.num_samples_per_class,
-                            replace = len(
-                                self.C_index[class_] < \
-                                        self.num_samples_per_class
-                                )
-                            )
-                        )
-                else:
-                    indices.extend(
-                        self.C_index[class_][self.C_count[class_]:
-                        self.C_count[class_] + self.num_samples_per_class]
-                    )
-                self.C_count[class_] += self.num_samples_per_class
-                if self.C_count[class_] + self.num_samples_per_class \
-                        > len(self.C_index[class_]):
-                    np.random.shuffle(self.C_index[class_])
-                    self.C_count[class_] = 0
-            yield indices
-            self.count += self.num_classes * self.num_samples_per_class
+        for _ in range(self.sampler_length):
+            subset = []
+            ### Random Subset from Random classes
+            for _ in range(self.batch_size//self.samples_per_class):
+                class_key = random.choice(list(self.image_dict.keys()))
+                subset.extend([random.choice(self.image_dict[class_key])[-1] for _ in range(self.samples_per_class)])
+            yield subset
 
     def __len__(self):
-        return len(self.dataset) // self.batch_size
+        return self.sampler_length
 
