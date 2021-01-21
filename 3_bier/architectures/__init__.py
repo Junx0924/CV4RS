f4972095bbabe92f754ecbdd60bb1ec2c882d7a6
junx0924 <glamorbree@gmail.com>
2021-01-21 08:06:20 +0100

    delete unnecessary files

--- 3_bier/architectures/__init__.py
+++
@@ -1,8 +0,0 @@
-import architectures.resnet50
-import architectures.deep_inception
-
-def select(arch):
-    if  'resnet' in arch:
-        return resnet50.ResNet50
-    elif  'bninception' in arch:
-        return deep_inception.GoogleNet
