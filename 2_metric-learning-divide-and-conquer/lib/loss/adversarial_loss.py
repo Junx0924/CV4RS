79afecf4ed9c90fcbbb2a0c26f0f3a1d1e37f56e
junx0924 <glamorbree@gmail.com>
2021-01-23 20:09:15 +0100

    update adversarial loss

--- 2_metric-learning-divide-and-conquer/lib/loss/adversarial_loss.py
+++ 2_metric-learning-divide-and-conquer/lib/loss/adversarial_loss.py
@@ -3,47 +3,33 @@ import itertools
 
 
 """================================================================================================="""
-
-### MarginLoss with trainable class separation margin beta. Runs on Mini-batches as well.
 class adversarial(torch.nn.Module):
-    def __init__(self, sub_embed_sizes,hidden_adversarial_size,lambda_weight):
+    def __init__(self,hidden_adversarial_size,decorrnet_lr=0.00001):
         super(adversarial,self).__init__()
-        self.sub_dim  = sub_embed_sizes
-        self.weights    = lambda_weight
         self.proj_dim   = hidden_adversarial_size
+        self.lr   =  decorrnet_lr
 
-        temp =  list(itertools.combinations(sub_embed_sizes,2))
-        self.directions = [ str(i[0])+'-'+str(i[1])  for i in temp]
+    # source: shape (batch_size, dim1)
+    # target: shape (batch_size, dim2)
+    def forward(self, source, target):
+        assert len(source) == len(target)
+        # l2 normalize feature
+        source = F.normalize(source,p =2,dim =1)
+        target = F.normalize(target,p =2,dim =1)
+        
+        # Apply gradient reversal on input embeddings.
+        source =  torch.nn.functional.normalize(grad_reverse(source),dim=-1)  
+        target =  torch.nn.functional.normalize(grad_reverse(target),dim=-1) 
 
         #Projection network
-        self.regressors = nn.ModuleDict()
-        for direction in self.directions:
-            start_dim,end_dim =  direction.split('-')
-            self.regressors[direction] = torch.nn.Sequential(
-                 torch.nn.Linear(int(start_dim), self.proj_dim),
+        regressor = torch.nn.Sequential(
+                 torch.nn.Linear(len(source), self.proj_dim),
                  torch.nn.ReLU(), 
-                 torch.nn.Linear(self.proj_dim, int(end_dim))).to(torch.float).to(opt.device)
+                 torch.nn.Linear(self.proj_dim, len(target))).to(torch.float).to(opt.device)
 
-        #Learning Rate for Projection Network
-        self.lr        = opt.diva_decorrnet_lr
-
-    # feature: shape (batch_size, sz_embedding_size)
-    def forward(self, feature):
-        # l2 normalize feature
-        feature_dict ={}
-        for i in range(len(self.sub_dim)):
-            start = int(sum(self.sub_dim[:i]))
-            stop = int(start + self.sub_dim[i])
-            fvecs = F.normalize(feature[:, start:stop],p =2,dim =1)
-            feature_dict[str(self.sub_dim[i])]= fvecs
-
-        # Apply gradient reversal on input embeddings.
-        adj_feature_dict = {key:torch.nn.functional.normalize(grad_reverse(features),dim=-1) for key, features in feature_dict.items()}
         # Project one sub embedding to the space of the other (with normalization), then compute the correlation.
-        sim_loss = 0
-        for weight, direction in zip(self.weights, self.directions):
-            source, target = direction.split('-')
-            sim_loss += -1.*weight*torch.mean(torch.mean((adj_feature_dict[target]*torch.nn.functional.normalize(self.regressors[direction](adj_feature_dict[source]),dim=-1))**2,dim=-1))
+        sim_loss  = -1.0*torch.mean(torch.mean((target*torch.nn.functional.normalize(regressor(source]),dim=-1))**2,dim=-1))
+           
         return sim_loss
 
 
