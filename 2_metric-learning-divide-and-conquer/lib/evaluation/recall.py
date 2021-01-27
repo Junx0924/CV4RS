981b5cd754d27836dc8b9896c9e36705e893605d
junx0924 <glamorbree@gmail.com>
2021-01-27 12:48:04 +0100

    update recall for one-hot labels

--- 2_metric-learning-divide-and-conquer/lib/evaluation/recall.py
+++ 2_metric-learning-divide-and-conquer/lib/evaluation/recall.py
@@ -13,14 +13,19 @@ def calc_recall_at_k(T, Y, k):
     Y : [nb_samples x k] (k predicted labels/neighbours)
     """
     s = 0
-    for t, y in zip(T,Y):
-        t_labels = get_label(t) # true lables
-        y_labels = [get_label(yy) for yy in y[:k]] # predicted labels
-        y_labels = list(itertools.chain.from_iterable(y_labels))
-        y_labels = np.unique(y_labels)
-        ss = sum([1 for i in t_labels if i in y_labels])
-        # only count the sample which has all of true lables in the predicted labels
-        if ss == len(t_labels):
-            s = s + 1
+    if isinstance(T[0], int):
+        for t, y in zip(T,Y):
+            s = sum([1 for t, y in zip(T, Y) if t in y[:k]])
+    else:
+        # label is one-hot encoding
+        for t, y in zip(T,Y):
+            t_labels = get_label(t) # true lables
+            y_labels = [get_label(yy) for yy in y[:k]] # predicted labels
+            y_labels = list(itertools.chain.from_iterable(y_labels))
+            y_labels = np.unique(y_labels)
+            ss = sum([1 for i in t_labels if i in y_labels])
+            # only count the sample which has all of true lables in the predicted labels
+            if ss == len(t_labels):
+                s = s + 1
     return s / (1. * len(T))
 
