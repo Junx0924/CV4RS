c2bc4f525a194d1033fb83f19fa24304dd119831
junx0924 <glamorbree@gmail.com>
2021-01-27 13:25:21 +0100

    update

--- 1_DiVA_MultiFeature_DML/architectures/__init__.py
+++
@@ -1,8 +0,0 @@
-import architectures.multifeature_resnet50
-import architectures.multifeature_bninception
-
-def select(arch, opt):
-    if  'resnet50' in arch:
-        return multifeature_resnet50.Network(opt)
-    elif  'bninception' in arch:
-        return multifeature_bninception.Network(opt)
--- 3_bier/evaluate/nmi.py
+++
@@ -1,8 +0,0 @@
-import sklearn.cluster
-import sklearn.metrics.cluster
-
-def calculate(ys, xs_clustered):
-    """
-    normalized_mutual_information
-    """
-    return sklearn.metrics.cluster.normalized_mutual_info_score(xs_clustered, ys)
