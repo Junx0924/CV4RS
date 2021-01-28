5a0c576e19983a7df3ff890476ddfdb615ea04a6
junx0924 <glamorbree@gmail.com>
2021-01-28 22:24:02 +0100

    update

--- lib/loss/__init__.py
+++ lib/loss/__init__.py
@@ -2,30 +2,41 @@ from .margin_loss import MarginLoss
 from .adversarial_loss import Adversarial
 from .binominal_loss import BinomialLoss
 from .fast_moco import Fast_moco
-from .batchminner.semihard import Semihard
-from .batchminner.random_distance import random_distance
-from .batchminner.intra_random import intra_random
-from .batchminner.distance import distance
 
-def select(config,loss_name=''):
-    ds_name = config['dataset_selected']
-    nb_classes = int(config['transform_parameters'][ds_name]["classes"])
-   
-    if loss_name =='discriminative': 
-        criterion = MarginLoss(nb_classes,batchminer= distance())
-    elif loss_name =='selfsimilarity': 
+from .batchminner.semihard import Semihard
+from .batchminner.random_distance import Random_distance
+from .batchminner.intra_random import Intra_random
+from .batchminner.distance import Distance
+
+def select(config,to_optim,loss_name="",minner_name= ""):
+    batch_minner = select_batchminner(minner_name)
+    if 'margin' in loss_name:
+        ds_name = config['dataset_selected']
+        nb_classes = int(config['transform_parameters'][ds_name]["classes"])
+        criterion = MarginLoss(nb_classes,batchminner = batch_minner)
+        to_optim    += [{'params':criterion.parameters(), 'lr':criterion.beta_lr}]
+    elif 'moco' in loss_name: 
         criterion = Fast_moco(config) 
-    elif loss_name =='separation': 
-        criterion =Adversarial(config['hidden_adversarial_size']) 
-    elif loss_name == 'adversarial':
-        criterion =Adversarial(config['hidden_adversarial_size'],need_weight_loss= True) 
-    elif loss_name =='intra': 
-        criterion = MarginLoss(nb_classes,batchminer=  intra_random())  
-    elif loss_name =='shared':
-        criterion = MarginLoss(nb_classes,batchminer=  random_distance())
-    elif loss_name =='binominal':
+    elif  'adversarial' in loss_name : 
+        criterion = Adversarial(config['hidden_adversarial_size'],config['decorrelation']) 
+        if config['project'] =='bier':
+            criterion =Adversarial(config['hidden_adversarial_size'],config['decorrelation'],need_weight_loss= True) 
+        to_optim    += [{'params':criterion.parameters(), 'lr':criterion.lr}]
+    elif 'binominal' in loss_name : 
         criterion = BinomialLoss(config['alpha'],config['beta'], config['margin'])
     else:
-        criterion = MarginLoss(nb_classes,batchminer= Semihard())
-    return criterion.to(config['device'])
+        raise Exception('Loss {} not available!'.format(loss_name))
+    return criterion.to(config['device']), to_optim
                     
+def select_batchminner(name=""):
+    if name =='semihard' :
+        batchminner = Semihard()
+    elif name =='distance' :
+        batchminner = Distance()
+    elif name =='intra_random' :
+        batchminner = Intra_random()
+    elif name =='random_distance' :
+        batchminner = Random_distance()
+    else:
+        batchminner = None
+    return batchminner
