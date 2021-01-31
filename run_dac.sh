8c71ec63b0351660a4e748408d63d4960b39d18c
junx0924 <glamorbree@gmail.com>
2021-01-31 21:41:43 +0100

    update

--- run_dac.sh
+++ run_dac.sh
@@ -18,5 +18,5 @@
 source /home/users/j/jun0924/venv/bin/activate
 export LD_LIBRARY_PATH=/home/users/j/jun0924/lib
 module load nvidia/cuda/10.0
-#python /home/users/j/jun0924/CV4RS/train_dac.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "/home/users/j/jun0924/Training_Results" --group dac_bs50_frozen --nb-clusters 4 --use_hdf5 --batch-size 50 --frozen 
-python /home/users/j/jun0924/CV4RS/train_dac.py  --log_online --dataset MLRSNet  --project MLRSNet  --source_path "/scratch/CV4RS/Dataset" --save_path "/home/users/j/jun0924/Training_Results" --group dac_bs50_frozen --nb-clusters 8 --batch-size 50 --frozen 
+#python /home/users/j/jun0924/CV4RS/train_dac.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "/home/users/j/jun0924/Training_Results" --group dac_bs50_frozen --dac_nb_clusters 4 --use_hdf5 --batch-size 50 --frozen 
+python /home/users/j/jun0924/CV4RS/train_dac.py  --log_online --dataset MLRSNet  --project MLRSNet  --source_path "/scratch/CV4RS/Dataset" --save_path "/home/users/j/jun0924/Training_Results" --group dac_bs50_frozen --dac_nb_clusters 8 --batch-size 50 --frozen 
