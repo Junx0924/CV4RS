1ce2ac62bcc44d55d48e0af1fb7f8a3bad4d01b5
junx0924 <glamorbree@gmail.com>
2021-01-31 21:36:18 +0100

    update

--- run_diva.sh
+++ run_diva.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH -J dac_bignet
-#SBATCH -o /home/users/j/jun0924/log/dac_bignet.out
+#SBATCH -J diva
+#SBATCH -o /home/users/j/jun0924/log/diva.out
 
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
@@ -18,5 +18,5 @@
 source /home/users/j/jun0924/venv/bin/activate
 export LD_LIBRARY_PATH=/home/users/j/jun0924/lib
 module load nvidia/cuda/10.0
-python /home/users/j/jun0924/CV4RS/train_diva.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "/home/users/j/jun0924/Training_Results" --group diva_bs50_frozen --batch-size 50  --use_hdf5 --frozen  --diva_features discriminative
+#python /home/users/j/jun0924/CV4RS/train_diva.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "/home/users/j/jun0924/Training_Results" --group diva_bs50_frozen --batch-size 50  --use_hdf5 --frozen  --diva_features discriminative
 python /home/users/j/jun0924/CV4RS/train_diva.py  --log_online --dataset MLRSNet  --project MLRSNet  --source_path "/scratch/CV4RS/Dataset" --save_path "/home/users/j/jun0924/Training_Results" --group diva_bs50_frozen --batch-size 50  --frozen  --diva_features discriminative
