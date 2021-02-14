883d114a4782c6b8508d068e74dfa8d7b1b83745
junx0924 <glamorbree@gmail.com>
2021-02-14 14:29:20 +0100

    update scripts

--- run_dac.sh
+++ run_dac.sh
@@ -9,7 +9,7 @@
 #SBATCH --gres=gpu:tesla:1
 
 #SBATCH --mem=30G
-#SBATCH --time=40:00:00
+#SBATCH --time=50:00:00
 #SBATCH --partition=gpu
 
 #SBATCH --mail-type=ALL
@@ -18,5 +18,4 @@
 source /home/users/j/jun0924/venv/bin/activate
 export LD_LIBRARY_PATH=/home/users/j/jun0924/lib
 module load nvidia/cuda/10.0
-python /home/users/j/jun0924/CV4RS/train_dac.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "/home/users/j/jun0924/Training_Results" --group dac --dac_nb_clusters 4 --use_hdf5 --num_samples_per_class 6 --frozen 
-#python /home/users/j/jun0924/CV4RS/train_dac.py  --log_online --dataset MLRSNet  --project MLRSNet  --source_path "/scratch/CV4RS/Dataset" --save_path "/home/users/j/jun0924/Training_Results" --group dac --dac_nb_clusters 8 --num_samples_per_class 2 --frozen  --use_hdf5
+python /home/users/j/jun0924/CV4RS/train_dac.py  --log_online --dataset MLRSNet  --project MLRSNet  --source_path "/scratch/CV4RS/Dataset" --save_path "/home/users/j/jun0924/Training_Results" --group dac --dac_nb_clusters 8 --num_samples_per_class 4 --frozen  --use_hdf5 --eval_epoch 20 --nb_epochs 100
