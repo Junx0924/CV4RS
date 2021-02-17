f071a8bcb8a3e445314b381afa19f8331ebcf5ac
junx0924 <glamorbree@gmail.com>
2021-02-17 14:31:44 +0100

    for evluation of final model

--- checkpoint_loader.py
+++ checkpoint_loader.py
@@ -18,27 +18,24 @@ parser = setup_parameters(parser)
 args = vars(parser.parse_args())
 checkpoint_folder =  args.pop('checkpoint_folder')
 
-### CREATE A SUMMARY TEXT FILE
-summary_text = ""
-
 # load config
 config = pkl.load(open(checkpoint_folder + '/hypa.pkl','rb'))
-
+checkpoint = torch.load(config['checkfolder']+"/checkpoint_recall@1.pth.tar")
 # load initial model
 model = lib.multifeature_resnet50.Network(config)
+model.load_state_dict(checkpoint['state_dict'])
+_  = model.to(config['device'])
 
 # create dataloader for evaluation
-dl_train = lib.data.loader.make(config, model,'eval', dset_type = 'train')
 dl_query = lib.data.loader.make(config, model,'eval', dset_type = 'query')
 dl_gallery = lib.data.loader.make(config, model,'eval', dset_type = 'gallery')
 
-print("Evaluate inital model\n")
-summary_text += "Evaluate inital model\n"
-scores = lib.utils.evaluate_query_gallery(model, config, dl_query, dl_gallery, False, config['backend'],is_init=True, K=[1],metrics=config['eval_metric'])
-for key in scores.keys(): 
-    summary_text += "{} :{:.3f}\n".format(key, scores[key])
-with open(checkpoint_folder+'/evaluate_inital_model.txt','w') as summary_file:
-    summary_file.write(summary_text)
-
 print("Evaluate final model\n")    
-lib.utils.eval_final_model(model,config,dl_query,dl_gallery,checkpoint_folder)
+#### CREATE A SUMMARY TEXT FILE
+summary_text = ""
+summary_text += "Evaluate final model\n"
+scores = lib.utils.evaluate_query_gallery(model, config, dl_query, dl_gallery, False, config['backend'], K=[1],metrics=config['eval_metric'],recover_image=True) 
+for key in scores.keys(): 
+  summary_text += "{} :{:.3f}\n".format(key, scores[key])
+  with open(config['checkfolder']+'/evaluate_final_model.txt','w+') as summary_file:
+      summary_file.write(summary_text)
