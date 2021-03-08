3106af628668d1d3c533951462b2dd565b6b0458
junx0924 <glamorbree@gmail.com>
2021-03-08 14:58:06 +0100

    update samplerun

--- SampleRun.sh
+++ SampleRun.sh
@@ -2,22 +2,22 @@ source /home/users/j/jun0924/venv/bin/activate
 export LD_LIBRARY_PATH=/home/users/j/jun0924/lib
 module load nvidia/cuda/10.0
 ### for training
-python ./train_bier.py  --log_online --dataset MLRSNet  --project MLRSNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results" --group bier  --num_samples_per_class 2 --frozen  --use_npmem --eval_epoch 10 --nb_epochs 120
-python ./train_dac.py  --log_online --dataset MLRSNet  --project MLRSNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results" --group dac --dac_nb_clusters 8 --num_samples_per_class 2 --frozen  --use_npmem --eval_epoch 10 --nb_epochs 120
-python ./train_diva.py  --log_online --dataset MLRSNet  --project MLRSNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results" --group diva --num_samples_per_class 2 --use_npmem --frozen  --savename combin -eval_epoch 10 --nb_epochs 120
-python ./train_snca.py  --log_online --dataset MLRSNet  --project MLRSNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results" --group snca  --batch_size 50  --frozen   --use_npmem -eval_epoch 10 --nb_epochs 120
+python ./train_bier.py  --log_online --dataset MLRSNet  --project MLRSNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results" --savename 'bier' --group bier  --num_samples_per_class 2 --frozen  --use_npmem --eval_epoch 10 --nb_epochs 120
+python ./train_dac.py  --log_online --dataset MLRSNet  --project MLRSNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results" --savename 'dac' --group dac --dac_nb_clusters 8 --num_samples_per_class 2 --frozen  --use_npmem --eval_epoch 10 --nb_epochs 120
+python ./train_diva.py  --log_online --dataset MLRSNet  --project MLRSNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results"  --savename 'diva' --group diva --num_samples_per_class 2 --use_npmem --frozen   -eval_epoch 10 --nb_epochs 120
+python ./train_diva.py  --log_online --dataset MLRSNet  --project MLRSNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results"  --savename 'baseline' --group diva --num_samples_per_class 2 --use_npmem --frozen   -eval_epoch 10 --nb_epochs 120 --diva_features 'discriminative'
 
-python ./train_bier.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results" --group bier  --num_samples_per_class 2 --frozen  --use_npmem --eval_epoch 10 --nb_epochs 120
-python ./train_dac.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results" --group dac --dac_nb_clusters 8 --num_samples_per_class 2 --frozen  --use_npmem --eval_epoch 10 --nb_epochs 120
-python ./train_diva.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results" --group diva --num_samples_per_class 2 --use_npmem --frozen  --savename combin -eval_epoch 10 --nb_epochs 120
-python ./train_snca.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results" --group snca  --batch_size 120  --frozen   --use_npmem -eval_epoch 10 --nb_epochs 120
+python ./train_bier.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results"  --savename 'bier' --group bier  --num_samples_per_class 2 --frozen  --use_npmem --eval_epoch 10 --nb_epochs 120
+python ./train_dac.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results"  --savename 'dac' --group dac --dac_nb_clusters 8 --num_samples_per_class 2 --frozen  --use_npmem --eval_epoch 10 --nb_epochs 120
+python ./train_diva.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results"  --savename 'diva' --group diva --num_samples_per_class 2 --use_npmem --frozen    -eval_epoch 10 --nb_epochs 120
+python ./train_diva.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results"  --savename 'baseline' --group diva --num_samples_per_class 2 --use_npmem --frozen    -eval_epoch 10 --nb_epochs 120 --diva_features 'discriminative'
 
 ### for evaluation
-python ./evaluation.py --load_from_checkpoint "../Training_Results/MLRSNet/dac_s0" --source_path "/scratch/CV4RS/Dataset"
-python ./evaluation.py --load_from_checkpoint "../Training_Results/BigEarthNet/dac_s0" --source_path "/scratch/CV4RS/Dataset"
-python ./evaluation.py --load_from_checkpoint "../Training_Results/MLRSNet/snca_s0" --source_path "/scratch/CV4RS/Dataset"
-python ./evaluation.py --load_from_checkpoint "../Training_Results/BigEarthNet/snca_s0" --source_path "/scratch/CV4RS/Dataset"
-python ./evaluation.py --load_from_checkpoint "../Training_Results/MLRSNet/bier_s0" --source_path "/scratch/CV4RS/Dataset"
-python ./evaluation.py --load_from_checkpoint "../Training_Results/BigEarthNet/bier_s0" --source_path "/scratch/CV4RS/Dataset"
-python ./evaluation.py --load_from_checkpoint "../Training_Results/MLRSNet/combin" --source_path "/scratch/CV4RS/Dataset"
-python ./evaluation.py --load_from_checkpoint "../Training_Results/BigEarthNet/combin" --source_path "/scratch/CV4RS/Dataset"
+python ./evaluate_model.py --load_from_checkpoint "../Training_Results/MLRSNet/dac" --source_path "/scratch/CV4RS/Dataset"
+python ./evaluate_model.py --load_from_checkpoint "../Training_Results/BigEarthNet/dac" --source_path "/scratch/CV4RS/Dataset"
+python ./evaluate_model.py --load_from_checkpoint "../Training_Results/MLRSNet/baseline" --source_path "/scratch/CV4RS/Dataset"
+python ./evaluate_model.py --load_from_checkpoint "../Training_Results/BigEarthNet/baseline" --source_path "/scratch/CV4RS/Dataset"
+python ./evaluate_model.py --load_from_checkpoint "../Training_Results/MLRSNet/bier" --source_path "/scratch/CV4RS/Dataset"
+python ./evaluate_model.py --load_from_checkpoint "../Training_Results/BigEarthNet/bier" --source_path "/scratch/CV4RS/Dataset"
+python ./evaluate_model.py --load_from_checkpoint "../Training_Results/MLRSNet/diva" --source_path "/scratch/CV4RS/Dataset"
+python ./evaluate_model.py --load_from_checkpoint "../Training_Results/BigEarthNet/diva" --source_path "/scratch/CV4RS/Dataset"
