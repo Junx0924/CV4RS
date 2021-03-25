d67da663960ac3925ca1378e1802ee1df5e5d17e
junx0924 <glamorbree@gmail.com>
2021-03-25 02:53:23 +0100

    update samplerun.sh

--- SampleRun.sh
+++ SampleRun.sh
@@ -2,15 +2,15 @@ source /home/users/j/jun0924/venv/bin/activate
 export LD_LIBRARY_PATH=/home/users/j/jun0924/lib
 module load nvidia/cuda/10.0
 ### for training
-python ./train_bier.py  --log_online --dataset MLRSNet  --project MLRSNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results" --savename 'bier' --group bier  --num_samples_per_class 2 --frozen  --use_npmem --eval_epoch 10 --nb_epochs 120 --bier_beta_trainable
+python ./train_bier.py  --log_online --dataset MLRSNet  --project MLRSNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results" --savename 'bier' --group bier  --num_samples_per_class 2 --frozen  --use_npmem --eval_epoch 10 --nb_epochs 120  
 python ./train_dac.py  --log_online --dataset MLRSNet  --project MLRSNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results" --savename 'dac' --group dac --dac_nb_clusters 8 --num_samples_per_class 2 --frozen  --use_npmem --eval_epoch 10 --nb_epochs 120 
 python ./train_diva.py  --log_online --dataset MLRSNet  --project MLRSNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results"  --savename 'diva' --group diva --num_samples_per_class 2 --use_npmem --frozen   -eval_epoch 10 --nb_epochs 120 
-python ./train_diva.py  --log_online --dataset MLRSNet  --project MLRSNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results"  --savename 'baseline' --group baseline --num_samples_per_class 2 --use_npmem --frozen   -eval_epoch 10 --nb_epochs 120 --diva_features 'discriminative'
+python ./train_baseline.py  --log_online --dataset MLRSNet  --project MLRSNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results"  --savename 'baseline' --group baseline --num_samples_per_class 2 --use_npmem --frozen   --eval_epoch 10 --nb_epochs 120  
 
-python ./train_bier.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results"  --savename 'bier' --group bier  --num_samples_per_class 2 --frozen  --use_npmem --eval_epoch 10 --nb_epochs 120 --bier_beta_trainable
+python ./train_bier.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results"  --savename 'bier' --group bier  --num_samples_per_class 2 --frozen  --use_npmem --eval_epoch 10 --nb_epochs 120  
 python ./train_dac.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results"  --savename 'dac' --group dac --dac_nb_clusters 8 --num_samples_per_class 2 --frozen  --use_npmem --eval_epoch 10 --nb_epochs 120 
-python ./train_diva.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results"  --savename 'diva' --group diva --num_samples_per_class 2 --use_npmem --frozen    -eval_epoch 10 --nb_epochs 120
-python ./train_diva.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results"  --savename 'baseline' --group baseline --num_samples_per_class 2 --use_npmem --frozen    -eval_epoch 10 --nb_epochs 120 --diva_features 'discriminative'
+python ./train_diva.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results"  --savename 'diva' --group diva --num_samples_per_class 2 --use_npmem --frozen    --eval_epoch 10 --nb_epochs 120
+python ./train_baseline.py  --log_online --dataset BigEarthNet  --project BigEarthNet  --source_path "/scratch/CV4RS/Dataset" --save_path "../Training_Results"  --savename 'baseline' --group baseline --num_samples_per_class 2 --use_npmem --frozen    --eval_epoch 10 --nb_epochs 120  
 
 ### for evaluation
 python ./evaluate_model.py --load_from_checkpoint "../Training_Results/MLRSNet/dac" --source_path "/scratch/CV4RS/Dataset"
